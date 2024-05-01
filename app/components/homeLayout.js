0e32df979c690c745cab95a565a4c3b5c1352ce1
Nash <nashb1323@gmail.com>
2024-05-01 17:12:54 -0500

    added to home page

--- app/components/homeLayout.js
+++ app/components/homeLayout.js
@@ -1,6 +1,9 @@
-import Image from 'next/image'
+'use client';
 
-export default async function HomeLayout() {
+import Image from 'next/image'
+import { useIntersectionObserver } from '../hooks';
+
+export default function HomeLayout() {
   
   return (
         <div className="flex flex-col bg-base-100">
@@ -33,33 +36,167 @@ function Hero() {
 }
 
 function ProductHighlights() {
+    const { isIntersecting: squareIsIntersecting, ref: squareRef } = useIntersectionObserver({
+        threshold: 1,
+    })
+    
+    const { isIntersecting: easyToUseIsIntersecting, ref: easyToUseRef } = useIntersectionObserver({
+        threshold: 0.5,
+    })
+    
+    const { isIntersecting: pageBreakIsIntersecting, ref: pageBreakRef } = useIntersectionObserver({
+        threshold: 0.5,
+    })
+    
+    const { isIntersecting: lightweightIsIntersecting, ref: lightweightRef } = useIntersectionObserver({
+        threshold: 0.5,
+    })
+    
+    const { isIntersecting: parameterIsIntersecting, ref: parameterRef } = useIntersectionObserver({
+        threshold: 0.5,
+    })
+
+
+    console.log(`Render Section`, {
+        squareIsIntersecting
+    })
     return (
-        <div classname='flex flex-col bg-base-100 text-gray-500'>
-            <div className='flex justify-start pl-12 items-center text-gray-500'>
-                <div className='w-1/3'>
+        <div className='flex flex-col mb-80 items-center justify-center'>
+            <div className='relative flex h-fit'>
+                <ProductText
+                    easyToUseRef={easyToUseRef}
+                    pageBreakRef={pageBreakRef}
+                    lightweightRef={lightweightRef}
+                    parameterRef={parameterRef}
+                />
+                <ProductSquare 
+                    refs={squareRef}
+                    squareIsIntersecting={squareIsIntersecting}
+                    easyToUseIsIntersecting={easyToUseIsIntersecting}
+                    pageBreakIsIntersecting={pageBreakIsIntersecting}
+                    lightweightIsIntersecting={lightweightIsIntersecting}
+                    parameterIsIntersecting={parameterIsIntersecting}
+                />
+            </div>
+        </div>
+    )
+}
+
+function ProductText({easyToUseRef, pageBreakRef, lightweightRef, parameterRef}) {
+    return (
+        <div className='flex flex-col bg-base-100 text-gray-500'>
+            <div ref={easyToUseRef} className='flex justify-start h-screen items-center text-gray-500'>
+                <div className='w-1/2'>
                     <h1 className="text-5xl font-bold">Easy to use</h1>
                     <p className="py-6">Install the library, import the create table function, then print!</p>
                 </div>
-            </div>.
-            <div className='flex justify-start pl-12 items-center text-gray-500'>
-                <div className='w-1/3'>
+            </div>
+            <div ref={pageBreakRef} className='flex justify-start h-screen items-center text-gray-500'>
+                <div className='w-1/2'>
                     <h1 className="text-5xl font-bold">Handle Page Breakes with Ease</h1>
                     <p className="py-6">Pages can start with or without headers. Table demensions are returned facilitating dynamic footers to fill space</p>
                 </div>
             </div>.
-            <div className='flex justify-start pl-12 items-center text-gray-500'>
-                <div className='w-1/3'>
+            <div ref={lightweightRef} className='flex justify-start h-screen items-center text-gray-500'>
+                <div className='w-1/2'>
                     <h1 className="text-5xl font-bold">Lightweight & Preformant</h1>
                     <p className="py-6">Pdf-Lib-Table has no external dependances and can print hundreds of pages with ease</p>
                 </div>
             </div>
-            <div className='flex justify-start pl-12 items-center text-gray-500'>
-                <div className='w-1/3'>
+            <div ref={parameterRef} className='flex justify-start h-screen items-center text-gray-500'>
+                <div className='w-1/2'>
                     <h1 className="text-5xl font-bold">Over 50 parameters</h1>
                     <p className="py-6">Easy to get going, but enough customization to grow with your project</p>
                 </div>
             </div>
         </div>
+    )
+}
+
+function ProductSquare({refs, squareIsIntersecting, easyToUseIsIntersecting, pageBreakIsIntersecting, lightweightIsIntersecting, parameterIsIntersecting}) {
+    console.log(squareIsIntersecting)
+
+    return (
+        <div ref={refs} className={`sticky top-[calc(100vh/2-300px)] right-0 h-fit w-[750px] transform transition-all ease-in duration-700 ${easyToUseIsIntersecting | pageBreakIsIntersecting | lightweightIsIntersecting | parameterIsIntersecting ? 'opacity-100' : 'opacity-0 translate-x-32'}`}>
+            <div className="card w-full h-full bg-base-200 text-primary-content">
+                <div className="card-body">
+                    {
+                        easyToUseIsIntersecting ? <EasyToUse/> : pageBreakIsIntersecting ? <PageBreak/> : lightweightIsIntersecting ? <LightWeight/> : parameterIsIntersecting ? <Parameters/> : 'NOTHING TO SEE'
+                    }
+                </div>
+            </div>
+        </div>
+    )
+}
+
+function EasyToUse() {
+    return (
+        <div className="mockup-code">
+            <pre data-prefix="1"><code>{`import { createPDFTables } from 'pdf-lib-table';`}</code></pre> 
+            <pre data-prefix="2"><code></code></pre> 
+            <pre data-prefix="3"><code>{`//TABLE SETTINGS`}</code></pre> 
+            <pre data-prefix="4"><code>{`const pdfSettings = {`}</code></pre> 
+            <pre data-prefix="5"><code>     data,</code></pre>
+            <pre data-prefix="6"><code>     columns,</code></pre>
+            <pre data-prefix="7"><code>     pdfDoc</code></pre>
+            <pre data-prefix="8"><code>{`});`}</code></pre>
+            <pre data-prefix="9"><code></code></pre>
+            <pre data-prefix="10"><code></code></pre>
+            <pre data-prefix="11"><code>{`const tables = await createPDFTables(`}</code></pre>
+            <pre data-prefix="12"><code>     data</code></pre>
+            <pre data-prefix="13"><code>     page</code></pre>
+            <pre data-prefix="14"><code>     pdfDoc</code></pre>
+            <pre data-prefix="15"><code>     columnDefs</code></pre>
+            <pre data-prefix="16"><code>     StandardFonts</code></pre>
+            <pre data-prefix="17"><code>     rgb</code></pre>
+            <pre data-prefix="18"><code>     {`{pdfSettings}`}</code></pre>
+            <pre data-prefix="19"><code>{`);`}</code></pre>
+            <pre data-prefix="20"><code></code></pre>
+            <pre data-prefix="21"><code></code></pre>
+            <pre data-prefix="22"><code>{`tables.drawVerticalTables();`}</code></pre>
+        </div>
+    )
+}
+
+function PageBreak() {
+    return (
+        <div className='flex justify-center items-center'>
+            <Image
+                src="/continues-on-next-page.svg"
+                width={600}
+                height={600}
+                alt="Picture of the author"
+            />
+        </div>
+    
+    )
+}
+
+function LightWeight() {
+    return (
+        <div className='flex justify-center items-center h-[600px] bg-gray-800 rounded-md'>
+            <Image
+                src="/to-the-stars.svg"
+                width={600}
+                height={600}
+                alt="Picture of the author"
+            />
+        </div>
+    
+    )
+}
+
+function Parameters() {
+    return (
+        <div className='flex justify-center items-center h-[600px] bg-gray-800 rounded-md'>
+            <Image
+                src="/computer-program.svg"
+                width={600}
+                height={600}
+                alt="Picture of the author"
+            />
+        </div>
+    
     )
 }
 
